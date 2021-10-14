8656c185f6de303c34228ad3bf8fd2c86151e793
coboy <416212316@qq.com>
2021-10-14 14:58:14 +0800

    typescript learning

--- src/compiler-core/types.ts
+++ src/compiler-core/types.ts
@@ -16,13 +16,13 @@ interface IStaff {
   }
   
   const userMap2 = {
-    dev: Developer,
-    manager: Manager
+    dev: Developer2,
+    manager: Manager2
   };
   type Keys2 = keyof typeof userMap2; // 'dev' | 'manager'
   type userTypes2 = typeof userMap2[Keys2]; //typeof Developer | typeof Manager
   type ExtractInstanceType2<T> = T extends new () => infer R ? R : never;
-  
+//   type a = typeof Developer2 | typeof Manager2 
   class UserFactory2 {
     static getUser(k: Keys2): ExtractInstanceType2<userTypes2> {
       return new userMap2[k]();
@@ -42,4 +42,68 @@ interface IStaff {
   console.log("salary for dev: ", new UserService2().getSalaryByUser("dev"));
 
   const obj = { a: 1, b: 'xx' }
-  type keyObj = typeof obj
+  type keyObj = keyof typeof obj
+
+
+//   type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+
+function t() {
+
+}
+type tt = typeof t
+
+type at = {b: number, c: string}
+
+const a: at = {b: 1, c: 'xx'}
+const b: at = {b: 2, c: 'oo'}
+type aa = keyof typeof a
+
+type aa1 = keyof typeof b
+
+// 泛型
+type Dog<T> = { name: string, type: T }
+
+const dog: Dog<number> = { name: 'litter dog', type: 1 }
+
+function addDog<T>(dog: Dog<T>) { return dog }
+
+// 泛型约束
+type n = number
+
+function sum(arr: number[]): number {
+    let count = 0
+    arr.forEach(val => {
+        count = count + val
+    });
+    return count
+}
+
+sum([1, 2, 3])
+
+type D = { name: string, age: number }
+
+function test<T extends D>(d:T): T {
+    return d
+}
+
+const d1 = { name: 'd1', age: 1, type: 2 }
+test(d1)
+test({name: 'tt', age: 3})
+
+// 支持多个泛型参数
+
+function pick<T, U extends T>() {}
+
+type DD = { name: string, age: number }
+type DD1 = { name: string, age: number, type: number }
+
+pick<DD, DD1>()
+
+// extends 也可以当做一个三元运算符来使用
+
+function tt<T, U>(v: T extends U ? string : number) {}
+tt<DD1, DD>('sdff')
+
+// 泛型推断 infer
+type Foo<T> = T extends { t: infer T1 } ? T1 : number
+type F2 = Foo<{t: 'xxx'}>
